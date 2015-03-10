f221a6604a5c363ad8f4a9a27c5fe42ac5f6f2b8
kvipka <qvipka@gmail.com>
2015-03-10 12:03:02 +0300

    sql corrections for easy install)

---
+++ sql/RE_updates/character_groups.sql
@@ -0,0 +1,62 @@
+-- ----------------------------
+-- Table structure for `group_instance`
+-- ----------------------------
+DROP TABLE IF EXISTS `group_instance`;
+CREATE TABLE `group_instance` (
+  `leaderGuid` int(11) unsigned NOT NULL DEFAULT '0',
+  `instance` int(11) unsigned NOT NULL DEFAULT '0',
+  `permanent` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`leaderGuid`,`instance`),
+  KEY `instance` (`instance`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of group_instance
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `group_member`
+-- ----------------------------
+DROP TABLE IF EXISTS `group_member`;
+CREATE TABLE `group_member` (
+  `groupId` int(11) unsigned NOT NULL,
+  `memberGuid` int(11) unsigned NOT NULL,
+  `memberFlags` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'Member flags bit mask',
+  `subgroup` smallint(6) unsigned NOT NULL,
+  `roles` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'Member roles bit mask',
+  PRIMARY KEY (`groupId`,`memberGuid`),
+  KEY `Idx_memberGuid` (`memberGuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Groups';
+
+-- ----------------------------
+-- Records of group_member
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `groups`
+-- ----------------------------
+DROP TABLE IF EXISTS `groups`;
+CREATE TABLE `groups` (
+  `groupId` int(11) unsigned NOT NULL,
+  `leaderGuid` int(11) unsigned NOT NULL,
+  `lootMethod` tinyint(4) unsigned NOT NULL,
+  `looterGuid` int(11) unsigned NOT NULL,
+  `lootThreshold` tinyint(4) unsigned NOT NULL,
+  `icon1` int(11) unsigned NOT NULL,
+  `icon2` int(11) unsigned NOT NULL,
+  `icon3` int(11) unsigned NOT NULL,
+  `icon4` int(11) unsigned NOT NULL,
+  `icon5` int(11) unsigned NOT NULL,
+  `icon6` int(11) unsigned NOT NULL,
+  `icon7` int(11) unsigned NOT NULL,
+  `icon8` int(11) unsigned NOT NULL,
+  `groupType` tinyint(1) unsigned NOT NULL,
+  `difficulty` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `raiddifficulty` int(11) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`groupId`),
+  UNIQUE KEY `leaderGuid` (`leaderGuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Groups';
+
+-- ----------------------------
+-- Records of groups
+-- ----------------------------
